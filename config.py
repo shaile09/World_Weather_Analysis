6629f7ec1de56111c9ceae28ab906611b2131b85
emailseghen@gmail.com <Kahid3lu!>
2020-01-05 16:08:33 -0500

    update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #weather API
-weather_api_key = "e8b7b8ba89861889e4baf0d754ccb2c2"
+weather_api_key = "f8a8f3743cd9de275d97a88345ed6688"
 
 #google API
 g_key = "AIzaSyCxawQOa57WhaF5DvTFUfWnnIsLJs-1mVw"
